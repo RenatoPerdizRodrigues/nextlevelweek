7db26d307e1f58daab391ce33f42f90b4fc2a609
Renato Perdiz Rodrigues <renato@proxy.com.br>
2020-06-02 18:54:54 -0300

    testing some routes and params

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,41 @@
-import express from 'express';
+import express, { response } from 'express';
 
 //Devemos trazer não só o código, mas também sua definição de tipo
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log(request.query.name);
+//Definimos que é JSON
+app.use(express.json());
 
-    response.json({"msg":"userlist","user_list":[{"name":"Renato"},{"name":"Lorran"}]});
+const users =["Renato","Lorran","Erick"];
+
+app.get('/users', (request, response) => {
+
+    const search = String(request.query.search);
+    
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    response.json(user);
+});
+
+app.get('/', (request, response) => {
+    response.json({"yee":"haaw"});
+});
+
+app.post('/users', (request,response) => {
+    const user = {
+        name: request.body.name,
+        email: request.body.email,
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);

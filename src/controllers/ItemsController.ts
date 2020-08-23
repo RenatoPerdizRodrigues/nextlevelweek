c7443e9f15af568ee7c78067f4520c950f6c2702
Renato Perdiz Rodrigues <renato@proxy.com.br>
2020-08-23 11:17:50 -0300

    master: projeto mobile com duas páginas

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -14,7 +14,7 @@ class ItemsController{
                 id: item.id,
                 title: item.title,
                 //Temos que usar JAPANESE ASPAS por algum motivo
-                image_url: `http://localhost:3333/uploads/${item.image}`,
+                image_url: `http://192.168.0.18/uploads/${item.image}`,
             }
         });
     
